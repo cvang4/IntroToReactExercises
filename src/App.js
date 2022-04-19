e47b21a99651ab38a8a64051a8fc3670b4f4ee33
Choua Vang <c_vang4@outlook.com>
2022-04-18 22:39:59 -0400

    second update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
 import React from 'react'
 
+//Exercises 1.3 - 1.5
+const App = () => {
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+};
+
+const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+        )
+      };
+
+
+const Content = (props) => {
+    return (
+      <div>
+        <p> {props.parts} </p>
+      </div>
+    )
+};
+
+const Total = (props) => {
+    return (
+      <div>
+        <p>Total number of exercises: parts={props.parts}</p>
+      </div>
+    )
+}
+
+
+export default App;
+
+
+{/* // Exercise 1.1 - 1.2
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -42,6 +101,4 @@ const Total = (props) => {
         <p>Total number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
       </div>
     )
-}
-
-export default App;
+} */}
